ee6126b9bf486969818f3f953c2259c39a05154e
Akhil Gautam <akhilgautam123@gmail.com>
2021-05-07 11:44:59 +0530

    Fixed copying of databas.yml file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,7 @@ echo "ENVIRONMENT: $RAILS_ENV"
 # remove any existing pid file
 rm -f $APP_PATH/tmp/pids/server.pid
 
+cp $APP_PATH/config/database.yml.postgresql $APP_PATH/config/database.yml
+
 # append bundle exec to every command
 bundle exec ${@}
