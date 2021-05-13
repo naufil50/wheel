f341d857ba5140782b32712c011496a226e5bf43
Akhil Gautam <akhilgautam123@gmail.com>
2021-05-13 13:45:58 +0530

    Added webpack as a separate container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,11 @@ echo "ENVIRONMENT: $RAILS_ENV"
 # remove any existing pid file
 rm -f $APP_PATH/tmp/pids/server.pid
 
-cp $APP_PATH/config/database.yml.postgresql $APP_PATH/config/database.yml
+database_config_path="$APP_PATH/config/database.yml"
+
+if [ ! -f "$database_config_path" ]; then
+  cp $APP_PATH/config/database.yml.postgresql "$database_config_path"
+fi
 
 # append bundle exec to every command
 bundle exec ${@}
